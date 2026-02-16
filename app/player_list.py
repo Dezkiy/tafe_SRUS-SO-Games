b6028ec8fa14d34210efd46b6d6a170e7f5a7f2b
Sam <orlov.sam@gmail.com>
2026-02-16 15:47:37 +0800

    add PlayerList class

---
+++ app/player_list.py
@@ -0,0 +1,76 @@
+class PlayerList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def is_empty(self):
+        return self.first is None
+        # return self.first is None and self.last is None
+    
+    def find_from_start(self, key):
+        current = self.first
+        while current and current != key:
+            current = current.next
+
+        return current
+        
+    def find_from_end(self, key):
+        current = self.last
+        while current and current != key:
+            current = current.previous
+
+        return current
+        
+    def insert_first(self, val):
+        new_node = Node(val)
+
+        if self.is_empty():
+            self.first = new_node
+            self.last = new_node
+            return
+        
+        new_node.next = self.first
+        self.first.previous = new_node
+        self.first = new_node
+
+    def insert_last(self, val):
+        new_node = Node(val)
+
+        if self.is_empty():
+            self.first = new_node
+            self.last = new_node
+            return
+        
+        new_node.previous = self.second
+        self.second.next = new_node
+        self.second = new_node
+
+    def delete(self, key):
+        current = self.first
+        previous = self.first
+
+        while current.val != key:
+            if current.next is None:
+                return None
+            else:
+                previous = current
+                current = current.next
+
+        if current == self.first:
+            self.first = self.first.next
+            self.first.previous = None
+        else:
+            previous.next = current.next
+            current.next.previous = previous
+
+        return current
+
+    def display(self):
+        print("List first --> last: ")
+        current = self.first
+        while current:
+            print("+++++++++++++++++++++")
+            print("prev: ", current.previous)
+            print("node => ", current)
+            print("next: ", current.next)
+            current = current.next
