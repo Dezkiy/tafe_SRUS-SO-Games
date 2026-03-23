0f5a545028ee6560e8dd5fd6fa0ebc741a5ad99a
Sam <orlov.sam@gmail.com>
2026-03-23 12:10:48 +0800

    test: update player repr expectations

--- test/player_test.py
+++ test/player_test.py
@@ -3,13 +3,18 @@ from app.player import Player
 
 class TestPlayer(unittest.TestCase):
     player_one = Player("1","Bob")
+    player_two = Player("2","Timofei", 2)
+
 
     def test_initialization(self):
         self.assertEqual(self.player_one._uid, "1")
-        self.assertEqual(self.player_one.name, "Bob")
+        self.assertEqual(self.player_one._name, "Bob")
 
-    def test_str(self):
-        self.assertEqual(str(self.player_one), "Player 1: Bob")
+    def test_repr_defaults_score_to_zero(self):
+        self.assertEqual(str(self.player_one), "Player(name='Bob', uid='1', score=0)")
+
+    def test_repr_includes_custom_score(self):
+        self.assertEqual(str(self.player_two), "Player(name='Timofei', uid='2', score=2)")
 
 if __name__ == "__main__":
     unittest.main()
